ea1a219c2ab840bba5ac15d56e7839090d0b9ccd
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-04-15 15:19:15 +0200

    Update FileSystemHelper.cs

--- Helpers/FileSystem/FileSystemHelper.cs
+++ Helpers/FileSystem/FileSystemHelper.cs
@@ -7,26 +7,14 @@ using Ben.Tools.Services;
 
 namespace Ben.Tools.Helpers.FileSystem
 {
-    // Erreur :
-    // Le chemmin n'est pas valide, trop long, contiend des caractères non voulues :
-    // - solution : path.Replace("/", "\\");
-    // Lorsque l'on travail avec des chemins de fichiers pointant sur le disque dur ":D" :
-    //•	Il faut utiliser ":D\" plutôt que ":D/". 
     public static class FileSystemHelper
     {
-         
         public static string SafeGetFileContent(string path) => 
             File.Exists(@path) ?
             File.ReadAllText(@path) :
             string.Empty;
 
-        public static void ShowFileContent(string path)
-        {
-            DebugService.Instance.Log(SafeGetFileContent(@path));
-        }
-
-        public static bool IsOfExtension(string path, string[] extensions) => 
-            extensions.Contains(Path.GetExtension(path));
+        public static bool IsOfExtension(string path, string[] extensions) => extensions.Contains(Path.GetExtension(path));
 
         public static void SafeCreatePath(string path)
         {
@@ -39,7 +27,7 @@ namespace Ben.Tools.Helpers.FileSystem
                     Directory.CreateDirectory(@pathDirectory);
 
 
-                if (!string.IsNullOrEmpty(@path) &&
+                if (!string.IsNullOrWhiteSpace(@path) &&
                     !File.Exists(@path))
                 {
                     using (var fileStream = System.IO.File.Create(@path))
@@ -56,39 +44,8 @@ namespace Ben.Tools.Helpers.FileSystem
             }
         }
 
-        public static IEnumerable<string> GetAllDirectoryFilesRecursively(
-            string directoryPath,
-            string[] extensions,
-            string[] exclusiveIgnoreFilters) => 
-            extensions.SelectMany(extension =>
-                Directory.GetFiles(@directoryPath, "*" + extension, SearchOption.AllDirectories))
-            .Where(fileName => !exclusiveIgnoreFilters.Contains(fileName));
+        public static string TakeOutExtension(string fileName) => Path.GetFileNameWithoutExtension(fileName);
 
-        public static void ListAllFilesRecursivelyInADirectory(
-            string directoryPath,
-            string[] extensions,
-            string[] exclusiveIgnoreFilters)
-        {
-            GetAllDirectoryFilesRecursively(directoryPath, extensions, exclusiveIgnoreFilters)
-                .Foreach(DebugService.Instance.Log);
-        }
-
-        public static int SubDirectoriesCount(string rootDirectory) => 
-            Directory.GetDirectories(@rootDirectory).Length;
-
-        public static bool IsEmptyFile(string path) => new FileInfo(@path).Length == 0;
-
-        public static string TakeOutExtension(string fileName) => Path.GetFileNameWithoutExtension(@fileName);
-
-        public static bool DoesPathIsAFileOrADirectory(string path) => !string.IsNullOrWhiteSpace(Path.GetExtension(@path));
-
-        public static string GetDesktopPath() => Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
-        public static long GetDirectoryBytesSize(string directoryPath) => 
-            Directory.GetFiles(@directoryPath, "*", SearchOption.AllDirectories)
-            .Sum(subFile => (new FileInfo(@subFile).Length));
-
-        public static bool IsReadonly(string path) => new FileInfo(path).IsReadOnly;
-        
+        public static string GetDesktopPath() => Environment.GetFolderPath(Environment.SpecialFolder.Desktop);   
     }
 }
