e9a103c107402f48279cb6ff064b43acda3915ed
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-04-17 21:18:37 +0200

    Update FileSystemHelper.cs

--- Helpers/FileSystem/FileSystemHelper.cs
+++ Helpers/FileSystem/FileSystemHelper.cs
@@ -7,14 +7,13 @@ using Ben.Tools.Services;
 
 namespace Ben.Tools.Helpers.FileSystem
 {
-    public static class FileSystemHelper
-    {
-        public static string SafeGetFileContent(string path) => 
+    public static string SafeGetFileContent(string path) => 
             File.Exists(@path) ?
             File.ReadAllText(@path) :
-            string.Empty;
+            "";
 
-        public static bool IsOfExtension(string path, string[] extensions) => extensions.Contains(Path.GetExtension(path));
+        public static bool IsOfExtension(string path, string[] extensions) => 
+            extensions.Contains(Path.GetExtension(path));
 
         public static void SafeCreatePath(string path)
         {
@@ -27,7 +26,7 @@ namespace Ben.Tools.Helpers.FileSystem
                     Directory.CreateDirectory(@pathDirectory);
 
 
-                if (!string.IsNullOrWhiteSpace(@path) &&
+                if (!string.IsNullOrEmpty(@path) &&
                     !File.Exists(@path))
                 {
                     using (var fileStream = System.IO.File.Create(@path))
@@ -44,8 +43,15 @@ namespace Ben.Tools.Helpers.FileSystem
             }
         }
 
-        public static string TakeOutExtension(string fileName) => Path.GetFileNameWithoutExtension(fileName);
+        public static IEnumerable<string> EnumerateFiles(
+            string directoryPath,
+            string[] extensions,
+            params string[] exclusiveIgnoreFilters) => 
+            extensions.SelectMany(extension =>
+                Directory.EnumerateFiles(directoryPath, "*", SearchOption.AllDirectories)
+                         .Where(filePath => extensions.Contains(Path.GetExtension(filePath)) &&
+                                            !exclusiveIgnoreFilters.Contains(filePath)));
 
-        public static string GetDesktopPath() => Environment.GetFolderPath(Environment.SpecialFolder.Desktop);   
+        public static string TakeOutExtension(string fileName) => Path.GetFileNameWithoutExtension(@fileName);
     }
 }
