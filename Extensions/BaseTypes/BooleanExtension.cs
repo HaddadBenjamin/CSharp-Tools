df5b1a1cc8919ab8b6ca3ad9ba19d4cca851100f
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-04 23:25:48 +0200

    Update BooleanExtension.cs

--- Extensions/BaseTypes/BooleanExtension.cs
+++ Extensions/BaseTypes/BooleanExtension.cs
@@ -3,5 +3,9 @@ namespace Ben.Tools.Extensions.BaseTypes
     public static class BooleanExtension
     {
         public static bool Inverse(this bool boolean) => boolean ^ true;
+        
+        public static bool AtLeastOneTrue(params bool[] booleans) => booleans.Any(element => element);
+
+        public static bool AllTrue(params bool[] booleans) => booleans.All(element => element);
     }
 }
