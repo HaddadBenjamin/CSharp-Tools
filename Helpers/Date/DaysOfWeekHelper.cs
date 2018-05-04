612f582e7f81ecea2516ad643b6f37b2358dffa5
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-04 23:50:42 +0200

    Update DaysOfWeekHelper.cs

--- Helpers/Date/DaysOfWeekHelper.cs
+++ Helpers/Date/DaysOfWeekHelper.cs
@@ -9,12 +9,8 @@ namespace Ben.Tools.Helpers.Date
     public static class DayOfWeeksHelper
     {
         public static IEnumerable<DayOfWeek> GetDayOfWeeksSorted() => 
-            ((DayOfWeek[])Enum.GetValues(typeof(DayOfWeek)))
-            .OrderBy(dayOfWeek => dayOfWeek.ToIndex());
-
-        public static IEnumerable<string> GetDayOfWeeksText() => 
-            GetDayOfWeeksSorted()
-            .Select(dayOfWeek => dayOfWeek.ToString());
+            EnumerationHelper.ToEnumerations<DayOfWeek>()
+                             .OrderBy(dayOfWeek => dayOfWeek.ToIndex());
     }
     
 }
