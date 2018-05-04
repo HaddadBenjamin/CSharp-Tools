05b44f3bfed7d3f49dd2bd317aa8dbc8c540240a
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 00:10:32 +0200

    Update JsonSerializerHelper.cs

--- Helpers/FileSystem/JsonSerializerHelper.cs
+++ Helpers/FileSystem/JsonSerializerHelper.cs
@@ -1,54 +1,37 @@
-﻿using System.IO;
+using System.IO;
 using System.Text;
 using Newtonsoft.Json;
 
 namespace Ben.Tools.Helpers.FileSystem
 {
-    /// <summary>
-    /// Peut tout désérializer, que ce soit des classes abstraites ou des interfaces.
-    /// Déduit automatiquement le type enfant même en spécifiant un type parent :
-    /// Deserialize<Mamiphere>(rawJson) désérializera en vrai le type du rawjson soit probablement un Elephant.
-    /// </summary>
     public static class JsonSerializerHelper
     {
-         
-        public static readonly JsonSerializerSettings SerializeEverythingSettings = new JsonSerializerSettings()
+      public static readonly JsonSerializerSettings SerializeEverythingSettings = new JsonSerializerSettings()
         {
             TypeNameHandling = TypeNameHandling.All // Permet de désérialiser les interfaces.
         };
 
-        public static void ToFile<TDataType>(
-            TDataType dataType,
-            string path,
-            bool indent = true)
+        public static void SaveInFile<SaveType>(SaveType saveType, string path, bool indent = true) =>
+            File.WriteAllText(path, ToJson(saveType, indent));
+
+        public static string ToJson<SerializeType>(SerializeType serializeType, bool indent = true) => 
+            JsonConvert.SerializeObject(
+                serializeType,
+                indent ? Formatting.Indented : Formatting.None,
+                SerializeEverythingSettings);
+
+        public static LoadedType LoadFromFile<LoadedType>(string path) => ToType<LoadedType>(File.ReadAllText(path));
+
+        public static TypeToLoad ToType<TypeToLoad>(byte[] bytes)
+                where TypeToLoad : class
         {
-            File.WriteAllText(
-                path,
-                ToJson(dataType, indent));
-        }
-
-        public static string ToJson<TDataType>(
-            TDataType dataType,
-            bool indent = true) => 
-        JsonConvert.SerializeObject(
-            dataType,
-            indent ? Formatting.Indented : Formatting.None,
-            SerializeEverythingSettings);
-
-        public static TDataType FromFile<TDataType>(string path) => ToType<TDataType>(File.ReadAllText(path));
-
-        public static TDataType ToType<TDataType>(byte[] byteArray)
-                where TDataType : class
-        {
-            using (var stream = new MemoryStream(byteArray))
+            using (var stream = new MemoryStream(bytes))
             using (var reader = new StreamReader(stream, Encoding.UTF8))
                 return JsonSerializer.Create()
-                                     .Deserialize(reader, typeof(TDataType)) as TDataType;
+                                     .Deserialize(reader, typeof(TypeToLoad)) as TypeToLoad;
         }
 
         public static TDataType ToType<TDataType>(string json) => 
             JsonConvert.DeserializeObject<TDataType>(json, SerializeEverythingSettings);
-
-        
     }
 }
