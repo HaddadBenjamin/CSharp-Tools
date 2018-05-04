0fc01447285a7493e5f1badd9ce98f042d2c1073
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 00:13:25 +0200

    Update PathHelper.cs

--- Helpers/FileSystem/PathHelper.cs
+++ Helpers/FileSystem/PathHelper.cs
@@ -1,24 +1,15 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace Ben.Tools.Helpers.FileSystem
 { 
     public static class PathHelper
     {
-         
         public static string GenerateUniquePath(string extension = ".txt") => $"{Path.GetTempPath()}{Guid.NewGuid()}{extension}";
 
         public static string GeneratePath(string filename, string extension = ".txt") => $"{Path.GetTempPath()}{filename}{extension}";
 
-        public static string AppDataRoaming(params string[] subPaths)
-        {
-            var appDataRoamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-
-            foreach (var subPath in subPaths)
-                appDataRoamingPath = Path.Combine(appDataRoamingPath, subPath);
-
-            return appDataRoamingPath;
-        }
+        public static string AppDataRoaming() => Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
         public static string Desktop() => Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
     }
