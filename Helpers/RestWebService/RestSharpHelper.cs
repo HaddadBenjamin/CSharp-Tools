b78c77fae5537fcf9badda91001ad1aa67121cc3
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 00:15:07 +0200

    Update RestSharpHelper.cs

--- Helpers/RestWebService/RestSharpHelper.cs
+++ Helpers/RestWebService/RestSharpHelper.cs
@@ -9,15 +9,7 @@ namespace Ben.Tools.Helpers.RestWebService
         /// <summary>
         /// Exemple : GetResponseRestAsDynamic("http://192.168.1.250:23456/services/api/messaging/");
         /// </summary>
-        public static dynamic GetResponseRestAsDynamic(
-            string restUrl,
-            Method restRequestType = Method.GET)
-        {
-            var restClient = new RestClient(restUrl);
-            var restRequest = new RestRequest("", restRequestType);
-
-            return JsonConvert.DeserializeObject(restClient.Execute(restRequest).Content);
-        }
-        
+        public static dynamic GetResponseRestAsDynamic(string restUrl, Method restRequestType = Method.GET) =>
+            JsonConvert.DeserializeObject(new RestClient(restUrl).Execute(new RestRequest("", restRequestType)).Content);
     }
 }
