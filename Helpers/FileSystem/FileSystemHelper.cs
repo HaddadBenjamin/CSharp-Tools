303856525e8670e27ff7ce65632700d633e0cc9e
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 00:05:48 +0200

    Update FileSystemHelper.cs

--- Helpers/FileSystem/FileSystemHelper.cs
+++ Helpers/FileSystem/FileSystemHelper.cs
@@ -9,50 +9,37 @@ namespace Ben.Tools.Helpers.FileSystem
 {
     public static class FileSystemHelper
     { 
-        public static string SafeGetFileContent(string path) => 
-                File.Exists(@path) ?
-                File.ReadAllText(@path) :
-                string.Empty;
+       public static string SafeGetFileContent(string path) => File.Exists(@path) ? File.ReadAllText(@path) : string.Empty;
 
-        public static bool IsOfExtension(string path, string[] extensions) => 
-            extensions.Contains(Path.GetExtension(path));
+        public static bool IsOfExtension(string path, string[] extensions) =>  extensions.Contains(Path.GetExtension(path));
 
         public static void SafeCreatePath(string path)
         {
             try
             {
-                var pathDirectory = Path.GetDirectoryName(@path);
+                var pathDirectory = Path.GetDirectoryName(path);
 
-                if (!string.IsNullOrEmpty(@pathDirectory) &&
-                    !Directory.Exists(@pathDirectory))
-                    Directory.CreateDirectory(@pathDirectory);
+                if (!string.IsNullOrEmpty(pathDirectory) &&
+                    !Directory.Exists(pathDirectory))
 
-
-                if (!string.IsNullOrEmpty(@path) &&
-                    !File.Exists(@path))
-                {
-                    using (var fileStream = System.IO.File.Create(@path))
-                        fileStream.Close();
-                }
+                if (!string.IsNullOrEmpty(path) &&
+                    !File.Exists(path))
+                    using (var fileStream = System.IO.File.Create(path));
             }
             catch (Exception exception)
             {
-                Console.WriteLine("Exception lancé durant la création d'un chemin : {0} au chemin {1}",
-                    exception.Message,
-                    path);
-
-                throw;
+                throw new Exception(string.Format("Exception lancé durant la création d'un chemin : {0} au chemin {1}", exception.Message, path));
             }
         }
 
         public static IEnumerable<string> EnumerateFiles(
             string directoryPath,
             string[] extensions,
-            params string[] exclusiveIgnoreFilters) => 
+            params string[] ignoreFilters) => 
             extensions.SelectMany(extension =>
                 Directory.EnumerateFiles(directoryPath, "*", SearchOption.AllDirectories)
                          .Where(filePath => extensions.Contains(Path.GetExtension(filePath)) &&
-                                            !exclusiveIgnoreFilters.Contains(filePath)));
+                                            !ignoreFilters.Contains(filePath)));
 
         public static string TakeOutExtension(string fileName) => Path.GetFileNameWithoutExtension(@fileName);
     }
