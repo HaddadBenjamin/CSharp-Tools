741d0221be03dc891dae837aa341525c7e5fdd60
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 13:19:22 +0200

    Update IntExtension.cs

--- Extensions/BaseTypes/IntExtension.cs
+++ Extensions/BaseTypes/IntExtension.cs
@@ -7,16 +7,23 @@ namespace Ben.Tools.Extensions.BaseTypes
 {
     public static class IntExtension
     {
-        public static bool Any(this int integer) => integer > 0;
-        
-        public static bool IsBetween(this int number, int minimum, int maximum) => number >= minimum && number <= maximum;
-        
+        public static bool NearlyEqual(this int left, int right, int epsilon) => Math.Abs(left - right) <= epsilon;
+
+        public static bool IsBetween(this int number, int minimum, int maximum) => number > minimum && number < maximum;
+
+        public static bool IsBetweenOrEqual(this int number, int minimum, int maximum) => number >= minimum && number <= maximum;
+
+        public static bool IsBetweenOrNearlyEqual(this int number, int minimum, int maximum, int epsilon) =>
+            number.IsBetween(minimum, maximum) ||
+            number.NearlyEqual(minimum, epsilon) ||
+            number.NearlyEqual(maximum, epsilon);
+
         /// <summary>
         /// SOME like Sql, return true if the values contains one element that verify the comparerOperator comparaison with the number.
         /// </summary>
-        public static bool Some(this int number, IEnumerable<int> values, EComparerOperator comparerOperator)
+        public static bool Some(this int number, IEnumerable<int> values, EComparerOperator comparerOperator, int epsilon = default(int))
         {
-            var comparerFunction = IntHelper.OperatorComparerFunction(comparerOperator);
+            var comparerFunction = IntHelper.OperatorComparerFunction(comparerOperator, epsilon);
 
             return values.Any(value => comparerFunction(number, value));
         }
@@ -24,14 +31,12 @@ namespace Ben.Tools.Extensions.BaseTypes
         /// <summary>
         /// return true if all the values verify the comparerOperator comparaison with number.
         /// </summary>
-        public static bool SomeAll(this int number, IEnumerable<int> values, EComparerOperator comparerOperator)
+        public static bool SomeAll(this int number, IEnumerable<int> values, EComparerOperator comparerOperator, int espilon = default(int))
         {
-            var comparerFunction = IntHelper.OperatorComparerFunction(comparerOperator);
+            var comparerFunction = IntHelper.OperatorComparerFunction(comparerOperator, espilon);
 
             return values.All(value => comparerFunction(number, value));
         }
-
-        public static bool IsBetween(this int number, int minimum, int maximum) => number >= minimum && number <= maximum;
 
         /// <summary>
         /// IN like Sql, return true if one element of the values is equal to number.
