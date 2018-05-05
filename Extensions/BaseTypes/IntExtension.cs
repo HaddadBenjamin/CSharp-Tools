8b56f4d9e5c1f6529647e54fd2d014c43d10d069
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 09:17:46 +0200

    Update IntExtension.cs

--- Extensions/BaseTypes/IntExtension.cs
+++ Extensions/BaseTypes/IntExtension.cs
@@ -1,7 +1,9 @@
-﻿namespace Ben.Tools.Extensions.BaseTypes
+namespace Ben.Tools.Extensions.BaseTypes
 {
     public static class IntExtension
     {
         public static bool Any(this int integer) => integer > 0;
+        
+        public static bool IsBetween(this int number, int minimum, int maximum) => number >= minimum && number <= maximum;
     }
 }
