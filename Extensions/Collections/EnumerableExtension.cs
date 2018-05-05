1044f1896fb532ca3ded83ae94264a562d2260eb
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 16:17:12 +0200

    Update EnumerableExtension.cs

--- Extensions/Collections/EnumerableExtension.cs
+++ Extensions/Collections/EnumerableExtension.cs
@@ -6,38 +6,46 @@ namespace Ben.Tools.Extensions.Collections
 {
     public static class EnumerableExtension
     {
-        public static void ForEachWithIndex<TDataType>(
-            this IEnumerable<TDataType> collection,
-            Action<TDataType, int> callback)
+        public static void Foreach<ElementType>(
+            this IEnumerable<ElementType> collection,
+            Action<ElementType> callback)
+        {
+            foreach (ElementType element in collection)
+                callback?.Invoke(element);
+        }
+
+        public static void ForEachWithIndex<ElementType>(
+            this IEnumerable<ElementType> collection,
+            Action<ElementType, int> callback)
         {
             var index = 0;
 
-            foreach (TDataType element in collection)
+            foreach (ElementType element in collection)
                 callback?.Invoke(element, index++);
         }
 
         /// <summary>
         /// Mélange de façon aléatoire une collection.
         /// </summary>
-        public static IEnumerable<TElement> Shuffle<TElement>(this IEnumerable<TElement> collection) =>
+        public static IEnumerable<ElementType> Shuffle<ElementType>(this IEnumerable<ElementType> collection) =>
             collection.OrderBy(element => Guid.NewGuid());
 
-        public static bool ContainsAll<TElementType>(
-            this IEnumerable<TElementType> collection,
-            IEnumerable<TElementType> elements) =>
+        public static bool ContainsAll<ElementType>(
+            this IEnumerable<ElementType> collection,
+            IEnumerable<ElementType> elements) =>
             collection.All(elements.Contains);
 
-        public static bool ContainsAny<TElementType>(
-            this IEnumerable<TElementType> collection,
-            IEnumerable<TElementType> elements) =>
+        public static bool ContainsAny<ElementType>(
+            this IEnumerable<ElementType> collection,
+            IEnumerable<ElementType> elements) =>
             collection.Any(elements.Contains);
 
-        public static TElementType RandomElement<TElementType>(this IEnumerable<TElementType> collection) =>
+        public static ElementType RandomElement<ElementType>(this IEnumerable<ElementType> collection) =>
             collection.Shuffle()
                       .FirstOrDefault();
-        
-        public static IEnumerable<TElementType> RandomElements<TElementType>(
-            this IEnumerable<TElementType> collection,
+                      
+        public static IEnumerable<ElementType> RandomElements<ElementType>(
+            this IEnumerable<ElementType> collection,
             int numbersOfElementsToGenerate = 10)
         {
             var random = new Random();
@@ -65,7 +73,7 @@ namespace Ben.Tools.Extensions.Collections
             .Take(numberOfColumns);
 
         /// <summary>
-        /// GroupByLines(3) : 
+        /// GroupByLines :
         ///                             [1, 4, 7]
         /// [1, 2, 3, 4, 5, 6, 7]  =>   [2, 5,  ]
         ///                             [3, 6,  ]
@@ -80,24 +88,35 @@ namespace Ben.Tools.Extensions.Collections
                     null)
             .Take(numberOfLines);
 
-        public static void Foreach<TDataType>(
-            this IEnumerable<TDataType> collection,
-            Action<TDataType> callback)
-        {
-            foreach (TDataType element in collection)
-                callback?.Invoke(element);
-        }
-        
-       
         /// <summary>
         /// [1, 2, 3]         [1, 4, 7]
         /// [4, 5, 6]   ==>   [2, 5, 8]
         /// [7, 8, 9]         [3, 6, 9]
         /// </summary>
-        public static IEnumerable<IEnumerable<TElementType>> Transpose<TElementType>(
-            this IEnumerable<IEnumerable<TElementType>> jaggedArray) => 
+        public static IEnumerable<IEnumerable<ElementType>> Transpose<ElementType>(
+            this IEnumerable<IEnumerable<ElementType>> jaggedArray) =>
             jaggedArray
-            .SelectMany(row => row.Select((element, index) => new { value = element, index = index }))
-            .GroupBy(element => element.index, element => element.value, (index, value) => value);
+                .SelectMany(row => row.Select((element, index) => new { value = element, index = index }))
+                .GroupBy(element => element.index, element => element.value, (index, value) => value);
+
+        /// <summary>
+        /// COLEASCE like Sql, return the firt element not null, if none are found return null.
+        /// </summary>
+        public static ElementType ColeasceGetFirstElementNotNull<ElementType>(this IEnumerable<ElementType> collection)
+            where ElementType : class =>
+            collection.FirstOrDefault(element => element != null);
+
+        /// <summary>
+        /// NULLIF like Sql, return null if all the elements are equals, if none around found return the first not null.
+        /// </summary>
+        public static ElementType NullIfAllEqual<ElementType>(this IEnumerable<ElementType> collection)
+            where ElementType : class =>
+            collection.All(element => element == collection.FirstOrDefault())
+                ? null
+                : collection.First(element => element != null);
+
+        public static IEnumerable<ElementType> RemoveNullElements<ElementType>(this IEnumerable<ElementType> collection)
+            where ElementType : class =>
+            collection.Where(element => element != null);
     }
 }
