3318bbdf4e7286f5ff2fcecbde1042ad3e019a22
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 13:22:59 +0200

    Create FloatHelper.cs

---
+++ Helpers/BaseTypes/FloatHelper.cs
@@ -0,0 +1,30 @@
+using Ben.Tools.Extensions.BaseTypes;
+using System;
+
+namespace Ben.Tools.Helpers.BaseTypes
+{
+    public static class FloatHelper
+    {
+        public static Func<float, float, bool> OperatorComparerFunction(EComparerOperator comparerOperator, float epsilon = Single.Epsilon)
+        {
+            Func<float, float, bool> comparer;
+
+            switch (comparerOperator)
+            {
+                case EComparerOperator.Less:        comparer = ((left, right) => left < right);  break;
+                case EComparerOperator.LessOrEqual:
+                case EComparerOperator.LessOrNearlyEqual:
+                                                    comparer = ((left, right) => left < right || left.NearlyEquals(right, epsilon)); break;
+                case EComparerOperator.Equal:
+                case EComparerOperator.NearlyEqual:
+                                                    comparer = ((left, right) => left.NearlyEquals(right, epsilon)); break;
+                case EComparerOperator.MoreOrEqual:
+                case EComparerOperator.MoreOrNearlyEqual:
+                                                    comparer = ((left, right) => left > right || left.NearlyEquals(right, epsilon)); break;
+                default:                            comparer = ((left, right) => left > right);  break;
+            }
+
+            return comparer;
+        }
+    }
+}
