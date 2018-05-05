4e41cba2f667b303b1cb9903d3babd67ac37bb5a
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-05-05 09:17:00 +0200

    Update FloatExtension.cs

--- Extensions/BaseTypes/FloatExtension.cs
+++ Extensions/BaseTypes/FloatExtension.cs
@@ -1,12 +1,10 @@
-﻿using System;
+using System;
 
 namespace Ben.Tools.Extensions.BaseTypes
 {
     public static class FloatExtension
     {
-        public static bool NearlyEquals(
-            this float left,
-            float right,
-            float epsilon = 0.01f) => Math.Abs(left - right) <= epsilon;
-    }
+        public static bool NearlyEquals(this float left, float right, float epsilon = 0.01f) => Math.Abs(left - right) <= epsilon;
+        
+        public static bool IsBetween(this float number, float minimum, float maximum) => number >= minimum && number <= maximum;
 }
