46958672f7cbdcecf77e536b68ec2a9bf0d5113f
Un passionné dans la foule <haddad.b.pro@gmail.com>
2018-06-14 11:48:46 +0200

    Update FlowExecutionHelper.cs

--- Helpers/FlowExecutionHelper.cs
+++ Helpers/FlowExecutionHelper.cs
@@ -6,9 +6,14 @@ namespace Ben.Tools.Helpers
 {
     public static class FlowExecutionHelper
     {
-        public static void Ifs(IEnumerable<(bool condition, Action action)> ifAndElseIfs, Action @else = null)
+        public static void IfsTree(IfTree root)
         {
-            var trueIf = ifAndElseIfs.FirstOrDefault(element => element.condition);
+            root.Execute();
+        }
+
+        public static void Ifs(IEnumerable<(bool condition, Action action)> ifElseIfs, Action @else = null)
+        {
+            var trueIf = ifElseIfs.FirstOrDefault(element => element.condition);
 
             if (trueIf.condition)
                 trueIf.action?.Invoke();
@@ -29,5 +34,47 @@ namespace Ben.Tools.Helpers
             do      action(index++);
             while   (condition(index));
         }
+    }
+
+    /// <summary>
+    /// Arbre de if, else if, else.
+    /// </summary>
+    public class IfTree
+    {
+        #region Fields
+        public IEnumerable<IfTree> Nodes = Enumerable.Empty<IfTree>();
+        public IEnumerable<(bool condition, Action action, IfTree childNode)> IfElseIfs = Enumerable.Empty<(bool, Action, IfTree)>();
+        public (Action action, IfTree childNode) Else;
+        #endregion
+
+        #region Constructor(s)
+        public IfTree(
+            IEnumerable<(bool condition, Action action, IfTree childNode)> ifElseIfs,
+            (Action action, IfTree childNode) @else)
+        {
+            IfElseIfs = ifElseIfs;
+            Else = @else;
+        }
+        #endregion
+
+        #region Public Behaviour(s)
+        public void Execute()
+        {
+            var trueIf = IfElseIfs.FirstOrDefault(element => element.condition);
+
+            if (trueIf.condition)
+            {
+                trueIf.action?.Invoke();
+
+                trueIf.childNode?.Execute();
+            }
+            else
+            {
+                Else.action?.Invoke();
+
+                Else.childNode?.Execute();
+            }
+        }
+        #endregion
     }
 }
