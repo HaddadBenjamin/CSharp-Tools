64f9ccd7637c879ef4bc914494c8d20e2e20df1a
Benjamin Haddad <haddad.b.pro@gmail.com>
2018-06-16 10:56:26 +0200

    MERGE

--- Services/Configurations/EnvironmentSection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ben.Tools.Services.Configurations
-{
-    public class EnvironmentSection
-    {
-        public string CurrentEnvironment { get; set; }
-        public string DefaultEnvironment { get; set; }
-    }
-}
